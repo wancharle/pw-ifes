92743f4078a11589200905ebde35c7ec96b7a33a
wancharle <wancharle.quirino@ifes.edu.br>
2018-03-15 22:01:21 -0300

    jogo da velha com pedido ok

---
+++ exercicios/Jogo da Vellha/pedido.js
@@ -0,0 +1,5 @@
+celulas = [
+	['D',' ','C'], // primeira linha que se acessa com celulas[0]
+	[' ','B',' '], // segunda linha que se acessa com celulas[1]
+	['A',' ','E'] // terceira linha que se acessa com celulas[2]
+];
