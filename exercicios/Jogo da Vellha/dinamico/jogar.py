ba502e4241f56977b250550dd22126f71fbd19f9
wancharle <wancharle.quirino@ifes.edu.br>
2018-03-16 21:50:00 -0300

    xml request dinamico com numeros aleatórios

---
+++ exercicios/Jogo da Vellha/dinamico/jogar.py
@@ -0,0 +1,7 @@
+
+import sys
+
+print sys.argv[1]
+
+arquivo = open('bancoDeDaDos.js',"w")
+
---
+++ exercicios/Jogo da Vellha/dinamico/pedido.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Content-Type: text/plain
+echo.
+echo [
+echo [" ", "0", " x"],
+echo [" ", "%RANDOM%", " "],
+echo [" ", " ", " "]
+echo ]
+ 
