e6de8a11eba942c040c01798d42e21b6f7615c45
wancharle sebastião quirino <wancharle@gmail.com>
2018-03-17 05:44:42 -0300

    ajustes

--- java-cgi/ola.java
+++ java-cgi/ola.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
+
 public class ola {
     public static void main(String[] arg){
-        // imprime o Ola mundo
+        // cabeçalhos padões
         System.out.print("Content-type: text/html\n\n");
+        System.out.print("<meta charset='utf-8' />\n");
+
+        // imprime o Ola mundo
         System.out.print("Ola mundo!<br><br>");
-       
-	// imprime o tipo request
-	String method = System.getenv("REQUEST_METHOD");
+        
+        imprimeVariaveisDeFormularios();	
+    }
+
+    public static void imprimeVariaveisDeFormularios(){
+        // imprime o tipo request
+        String method = System.getenv("REQUEST_METHOD");
         System.out.print("Metodo:"+method);
-
-
-        // imprime as variaveis GET ou POST
+       // imprime as variaveis GET ou POST
         if (method != null){
-                if (method.equals("GET")){
-	        String getVARS = System.getenv("QUERY_STRING").replaceAll("&","<br>");
-                System.out.print("<BR>Variaveis GET:<BR>"+getVARS);
-                }else{
-                        System.out.print("<BR>Conteudo POST:<BR>");
+                String getVARS = System.getenv("QUERY_STRING").replaceAll("&","<br>");
+                System.out.print("<BR><BR>Variáveis GET:<BR>"+getVARS);
+                
+                System.out.print("<BR><BR>Variáveis POST:<BR>");
+                if (method.equals("POST")){
                         Scanner s = new Scanner(System.in);
                         while (s.hasNext()){
-				String t=s.next().replaceAll("&","<BR>");
+                                String t=s.next().replaceAll("&","<BR>");
                                 System.out.print(t);
                         }
                 }
